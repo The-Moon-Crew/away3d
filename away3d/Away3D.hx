b6a5d45119c63ce14d76eb37e691fd133cb89d50
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-14 16:29:42 -0300

    Refactor versioning and add BuildStage enum
    Updated version constants and added BuildStage enum.

--- away3d/Away3D.hx
+++ away3d/Away3D.hx
@@ -1,15 +1,36 @@
 package away3d;
 
-/**
- * Helper class to track a few important library constants.
- */
+enum abstract BuildStage(String) to String
+{
+	var ALPHA = "alpha";
+	var BETA = "beta";
+	var RC = "rc";
+	var RELEASE = "stable";
+}
+
+@:final
 class Away3D
 {
-	public static inline var WEBSITE_URL:String = "http://www.away3d.com";
+	public static final WEBSITE_URL:String = "https://away3d.com";
+	public static final REPOSITORY_URL:String = "https://github.com/openfl/away3d";
 	
-	public static inline var MAJOR_VERSION:Int = 4;
-	
-	public static inline var MINOR_VERSION:Int = 1;
-	
-	public static inline var REVISION:Int = 6;
+	public static final MAJOR_VERSION:Int = 5;
+	public static final MINOR_VERSION:Int = 0;
+	public static final REVISION:Int = 0;
+	public static final STAGE:BuildStage = BuildStage.RELEASE;
+
+	public static var version(get, never):String;
+
+	public static inline function isAtLeast(major:Int, minor:Int = 0, revision:Int = 0):Bool
+	{
+		if (MAJOR_VERSION != major) return MAJOR_VERSION > major;
+		if (MINOR_VERSION != minor) return MINOR_VERSION > minor;
+		return REVISION >= revision;
+	}
+
+	private static inline function get_version():String
+	{
+		var base = '$MAJOR_VERSION.$MINOR_VERSION.$REVISION';
+		return STAGE == BuildStage.RELEASE ? base : '$base-$STAGE';
+	}
 }
